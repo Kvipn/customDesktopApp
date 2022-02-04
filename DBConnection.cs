0a8e66bc8b38556a77f0e0d12f2dc184d0fddeca
Mikita Kazlouski <kvipntv@gmail.com>
2022-02-04 12:42:30 +0300

    Добавьте файлы проекта.

---
+++ DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace customDesktopApp
+{
+    internal class DBConnection : DbContext
+    {
+        public DBConnection()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-ILIVC2L\SQLEXPRESS;Database=Shop;Trusted_Connection=true;");
+        }
+    }
+}
